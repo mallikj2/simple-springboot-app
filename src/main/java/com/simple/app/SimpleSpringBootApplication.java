41ad067fbb7bf3109f5a16b650f535661abdf6e3
mallikj2 <mallikj2@gmail.com>
2025-07-30 20:05:52 -0400

    Update SimpleSpringBootApplication.java

--- src/main/java/com/simple/app/SimpleSpringBootApplication.java
+++ src/main/java/com/simple/app/SimpleSpringBootApplication.java
@@ -1,5 +1,11 @@
 package com.simple.app;
 
-public class SimpleSpringBootApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class SimpleSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringBootApplication.class, args);
+    }
 }
